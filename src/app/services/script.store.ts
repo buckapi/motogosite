c92dea22d5eb96da4e72be97afd5353392f868b1
root <root@iPhone>
2022-06-12 06:56:53 -0400

    first commit

---
+++ src/app/services/script.store.ts
@@ -0,0 +1,15 @@
+
+ interface Scripts {
+    name: string;
+    src: string;
+}  
+export const ScriptStore: Scripts[] = [
+    {name: 'bootstrap', src: './assets/assets/js/bootstrap.min.js'},
+    {name: 'slick', src: './assets/assets/js/slick.min.js'},
+    {name: 'jquery.magnific-popup', src: './assets/assets/js/jquery.magnific-popup.min.js'},
+    {name: 'isotope', src: './assets/assets/js/isotope.pkgd.min.js'},
+    {name: 'jquery.inview', src: './assets/assets/js/jquery.inview.min.js'},
+    {name: 'jquery.nice-select', src: './assets/assets/js/jquery.nice-select.min.js'},
+    {name: 'wow', src: './assets/assets/js/wow.min.js'},
+    {name: 'main', src: './assets/assets/js/main.js'}
+];
